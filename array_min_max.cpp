b0f825ee7832f3822aaa5a17ee8d72b83eb3b382
Mehran Gul <arammehran123@gmail.com>
2022-09-13 23:06:35 +0530

    array added

---
+++ array_min_max.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int array[n];
+
+    for(int i = 0; i < n; i++){
+        cin >> array[i];
+    }
+    int max_num = INT16_MIN, min_num = INT16_MAX;
+
+    for (int i = 0; i < n; i++){
+        max_num = max(max_num, array[i]);
+        min_num = min(min_num, array[i]);
+
+        
+
+    } 
+    cout << "the max is " << max_num << endl;
+    cout << "the min is " << min_num << endl;
+
+   
+    return 0;
+}
--- rhombus_pattern.cpp
+++ rhombus_pattern.cpp
@@ -4,8 +4,7 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    for (int i = 1; i <= n; i++)
-    {
+    for (int i = 1; i <= n; i++){
         for (int j = 1; j <= (2*n -i); j++)
         {
             if (j<=(n-i))
